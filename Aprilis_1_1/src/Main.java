0a7b02cdf8c0b3a37ab4036aaaf6c7c464452342
MareksSlihta <162182125+MareksSlihta@users.noreply.github.com>
2024-04-02 14:33:49 +0300

    Netieku gala ar For ciklu

---
+++ Aprilis_1_1/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        homework1();
+    }
+
+    public static void homework1(){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadiet skaitli!");
+
+        int a = sc.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            System.out.println("sum a");
+        }
+    }
