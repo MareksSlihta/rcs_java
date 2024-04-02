d3c461f4a9cb7672876cb6b95e4c697afb69857c
MareksSlihta <162182125+MareksSlihta@users.noreply.github.com>
2024-04-02 15:06:46 +0300

    Joprojām par for ciklu

---
+++ Aprilis_2_1/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        homework1();
+    }
+
+    public static void homework1(){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadiet skaitli!");
+
+        int a = sc.nextInt();
+
+        for(int i = 0; i < a; i++){
+            System.out.println( a + ( a - 1) );
+        }
+    }
+}
